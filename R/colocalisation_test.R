8e8d031abce7b3297e63ecab1ecc6cea4067b85c
RobCrawford527 <rcrawford527@hotmail.co.uk>
2024-05-10 11:48:17 +0100

    Fixed problems with closest_neighbours and colocalisation_test. Added legend to plotting function.

--- R/colocalisation_test.R
+++ R/colocalisation_test.R
@@ -1,11 +1,11 @@
 #' Use Threshold To Decide Whether Spots Are Colocalised
 #'
-#' @param distances Data frame containing distances between pairs of spots
+#' @param distances List of two data frames containing distances between nearest pairs of spots
 #' @param method Method used to determine colocalisation. Options are "radius" (uses sum of spot radii) or "absolute" (sets an absolute distance)
 #' @param threshold Distance threshold (in nm) for determining colocalisation. Used if method = "absolute"
 #' @param multiplier Multiplier for sum of radii. Set lower for more strict colocalisation. Used if method = "radius"
 #'
-#' @return Data frame with an added column indicating whether each pair of spots is colocalised or not (TRUE or FALSE)
+#' @return List of two data frames with added columns indicating whether each pair of spots is colocalised or not (TRUE or FALSE)
 #' @export
 #'
 colocalisation_test <- function(distances,
@@ -19,27 +19,32 @@ colocalisation_test <- function(distances,
   if (method == "absolute"){
 
     # colocalised if distance is less than absolute threshold value
-    distances <- dplyr::mutate(distances,
-                               colocalised = ifelse(distance < threshold, TRUE, FALSE))
+    distances <- lapply(distances, function(x) dplyr::mutate(x,
+                                                             colocalised = ifelse(distance < threshold, TRUE, FALSE)))
 
   } else if (method == "radius"){
 
     # colocalised if distance is less than sum of radii
     # multiplier is used to make threshold more strict
-    distances <- dplyr::mutate(distances,
-                               colocalised = ifelse(distance < fwhm_sum * multiplier, TRUE, FALSE))
+    distances <- lapply(distances, function(x) dplyr::mutate(x,
+                                                             colocalised = ifelse(distance < fwhm_sum * multiplier, TRUE, FALSE)))
 
   }
 
   # calculate the percentage colocalisation
   # print to the console
-  colocalisation <- round(sum(distances[["colocalised"]]) / nrow(distances) * 100, 1)
+  colocalisation <- lapply(distances, function(x) round(sum(x[["colocalised"]]) / nrow(x) * 100, 1))
   print(paste0(colocalisation,
-              "% of spots are colocalised, using ",
+              "% of spots in channel 1 are colocalised with a spot in channel 2, using ",
               ifelse(method == "absolute",
                      paste0("method = absolute with threshold of ", threshold, " nm"),
                      paste0("method = radius with multiplier of ", multiplier, " nm"))))
+  print(paste0(colocalisation,
+               "% of spots in channel 2 are colocalised with a spot in channel 1, using ",
+               ifelse(method == "absolute",
+                      paste0("method = absolute with threshold of ", threshold, " nm"),
+                      paste0("method = radius with multiplier of ", multiplier, " nm"))))
 
-  # return mutated data frame
+  # return mutated list
   distances
 }
--- R/plot_spots.R
+++ R/plot_spots.R
@@ -26,7 +26,9 @@ plot_spots <- function(spots_1 = NULL, spots_2 = NULL, outlines, cell_of_interes
     ggplot2::coord_equal() +
     ggplot2::theme_classic() +
     ggplot2::theme(strip.background = ggplot2::element_blank(),
-                   strip.text = ggplot2::element_blank())
+                   strip.text = ggplot2::element_blank()) +
+    ggplot2::scale_colour_manual(breaks = c("spots 1", "spots 2"),
+                                 values = c("spots 1" = colours[1], "spots 2" = colours[2]))
 
   # define colours
   colours <- viridis::viridis(n = 2, begin = 0.25, end = 0.75, direction = -1, option = "inferno")
@@ -36,15 +38,13 @@ plot_spots <- function(spots_1 = NULL, spots_2 = NULL, outlines, cell_of_interes
   if (!is.null(spots_1)){
     plot <- plot +
       ggplot2::geom_point(data = spots_1,
-                          mapping = ggplot2::aes(x = x_pos, y = -y_pos, group = cell),
-                          colour = colours[1])
+                          mapping = ggplot2::aes(x = x_pos, y = -y_pos, group = cell, colour = "spots 1"))
   }
   # spots_2
   if (!is.null(spots_2)){
     plot <- plot +
       ggplot2::geom_point(data = spots_2,
-                          mapping = ggplot2::aes(x = x_pos, y = -y_pos, group = cell),
-                          colour = colours[2])
+                          mapping = ggplot2::aes(x = x_pos, y = -y_pos, group = cell, colour = "spots 2"))
   }
 
   # return plot
